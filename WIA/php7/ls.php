9c5afaa6a73ca046f7822bd9723492559193b0d3
Dawid <dawid.stasiak17@gmail.com>
2020-11-02 14:19:12 +0100

    PHP 7 v2

--- WIA/php7/ls.php
+++ WIA/php7/ls.php
@@ -1,5 +1,7 @@
 <?php
-$d = dir("./");echo "Handle: ".$d->handle."<br>\n";echo "Path: ".$d->path."<br>\n";
+$d = dir("./");
+echo "Handle: ".$d->handle."<br>\n";
+echo "Path: ".$d->path."<br>\n";
 while($entry=$d->read()) {
     echo $entry."<br>\n";
 }
