cb92bcef8971ca8428583ceae4195f28eddd55f4
Dawid <dawid.stasiak17@gmail.com>
2020-11-04 20:20:40 +0100

    PBD 4

--- TSIAI/JS/6/znaki.js
+++ TSIAI/JS/6/znaki.js
@@ -3,8 +3,8 @@ function znaki(typPola = "input"){
     for (let i = 0; i<inputy.length; i++){
         const left = inputy[i].getAttribute("maxlength") - inputy[i].value.length;
         const pos = i +1;
-        const pole = document.getElementById("pole"+pos);
-        pole.innerHTML = "<div>Dla pola "+ pos +" pozostało "+left+" </div>";
+        const pole = document.getElementById("pole" + pos);
+        pole.innerHTML = "<div>Dla pola "+ pos +" pozostało " + left + " </div>";
         pole.style.color = left === 0 ? "red" : "lightgreen";
     }
 }
