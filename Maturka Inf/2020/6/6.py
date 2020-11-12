0048bfb412a3dc62f038348ac62c6eef38903372
Dawid <dawid.stasiak17@gmail.com>
2020-11-12 23:29:19 +0100

    Matura 2020 inf 6.1 poprawka

--- Maturka Inf/2020/6/6.py
+++ Maturka Inf/2020/6/6.py
@@ -7,6 +7,7 @@ lines.remove("data	port	towar	Z/W	ile ton	cena za tone w talarach")
 
 def first():
     towar = [0, 0, 0, 0, 0]
+    sumaTowar = [0, 0, 0, 0, 0]
     biggest = 0
     name = 0
     for index in range(len(lines)):
@@ -15,19 +16,24 @@ def first():
         if data[3] == "Z":
             if data[2] == 'T1':
                 towar[0] += 1
+                sumaTowar[0] += int(data[4])
             if data[2] == 'T2':
                 towar[1] += 1
+                sumaTowar[1] += int(data[4])
             if data[2] == 'T3':
                 towar[2] += 1
+                sumaTowar[2] += int(data[4])
             if data[2] == 'T4':
                 towar[3] += 1
+                sumaTowar[3] += int(data[4])
             if data[2] == 'T5':
                 towar[4] += 1
+                sumaTowar[4] += int(data[4])
     for index in range(len(towar)):
         if towar[index] > biggest:
             biggest = towar[index]
             name = index + 1
-    print("6.1: ",str(biggest) + " T" + str(name))
+    print("6.1: ",str(biggest) + " T" + str(name) + "\nSuma załadunków to " +str(sumaTowar[name]))
 
 
 def second():
